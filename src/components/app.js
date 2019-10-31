d3a019d696e895e94edb9a317ab17a74ee0f8268
zeybek <abdulkadirzeybek95@gmail.com>
2019-10-31 16:38:59 +0300

    proje hazirlandi-npm redux / react redux

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React  from 'react'
+import ReactDOM from 'react-dom'
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//       <h1>Bismillah...</h1>        
+//       </div>
+//     )
+//   }
+// }
+
+
+const App = () => {
+  return(
+   <div>
+     <h1>Bismillah</h1>
+   </div> 
+  )
+}
+
+export default App
